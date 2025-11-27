20693bf86867ac63804c949fad5ca5bc7c03f213
Aspearung <151929974+Aspearung@users.noreply.github.com>
2025-11-27 13:43:11 +0900

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    char *str = "dream program";
+    printf("문자열 %s이다. \n\n",str);
+    str = "computer dream program";
+    printf("문자열 %s 이다. \n\n",str);
+    return 0;
+}
